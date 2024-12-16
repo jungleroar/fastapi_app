93ef8de9730a3b77074661b0e6aca171c2851762
onemanzerg <donnydeus@gmail.com>
2024-12-16 11:18:38 +0300

    add sqlalchemy join for booking room

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,9 +1,42 @@
 from app.dao.base import BaseDAO
 from app.database import async_session_maker
-from sqlalchemy import select
+from sqlalchemy import select, func, and_, or_
 from app.bookings.models import Bookings
+from app.rooms.models import Rooms
+from datetime import date
 
 class BookingDAO(BaseDAO):
     model = Bookings
 
+    @classmethod
+    async def add(cls,
+                  user_id: int,
+                  room_id: int,
+                  date_from: date,
+                  date_to: date,
+                  ):
+        async with async_session_maker() as session:
 
+            booked_rooms = select(Bookings).where(
+                and_(
+                    Bookings.room_id == 1,
+                    or_(
+                        and_(
+                            Bookings.date_from >= date_from,
+                            Bookings.date_from <= date_to
+                        ),
+                        and_(
+                            Bookings.date_from <= date_from,
+                            Bookings.date_to > date_from
+                        ), 
+                    )
+                )
+            )
+
+            rooms_left = select(Rooms.quantity - func.count(booked_rooms.c.room_id)
+                                ).select_from(Rooms).join(
+                                    booked_rooms, booked_rooms.c.room_id == Rooms.id
+                                ).where(Rooms.id == 1).group_by(
+                                    Rooms.quantity, booked_rooms.c.room_id
+                                )
+            
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -4,6 +4,7 @@ from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
 from app.users.models import Users
 from app.users.dependencies import get_current_user
+from datetime import date
 
 router = APIRouter(
     prefix="/bookings",
@@ -11,5 +12,14 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings(user: Users = Depends(get_current_user)):
-    return await BookingDAO.find_all(user_id=1)
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
+    return await BookingDAO.find_all(user_id=user.id)
+
+
+@router.post("")
+async def add_booking(room_id: int,
+                      date_from: date,
+                      date_to: date,
+                      user: Users = Depends(get_current_user),
+                      ):
+    await BookingDAO.add(user.id, room_id, date_from, date_to)
