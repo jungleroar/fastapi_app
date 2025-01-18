c596942c014a3bbd55ce63414d450a79265772c7
onemanzerg <donnydeus@gmail.com>
2025-01-18 17:27:17 +0300

    add fixtures

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -9,6 +9,9 @@ from app.bookings.models import Bookings
 from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
 from app.users.models import Users
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+from app.main import app as fastapi_app
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -51,3 +54,16 @@ def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope="function")
+async def ac():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="function")
+async def session():
+    async with async_session_maker() as session:
+        yield session
+        
--- app/tests/unit_tests/test_users/test_api.py
+++ app/tests/unit_tests/test_users/test_api.py
@@ -1,3 +1,5 @@
+import time
+
 def test_abc():
     assert 1 == 1
     
