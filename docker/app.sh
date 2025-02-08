eddbabdd129a1f1214b1b49c4759322a5483f5a7
onemanzerg <donnydeus@gmail.com>
2025-02-08 19:37:58 +0300

    bash migrations

--- docker/app.sh
+++ docker/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Запуск миграций..."
-alembic upgrade head || { echo "Ошибка при выполнении миграций"; exit 1; }
+alembic upgrade head || { echo "Ошибка при выполнении миграций."; exit 1; }
 
 # Запуск приложения
 gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
