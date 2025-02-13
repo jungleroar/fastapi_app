a97b52ad95e996459b866fac152a393255116385
onemanzerg <donnydeus@gmail.com>
2025-02-13 23:23:02 +0300

    docker done

--- docker/app.sh
+++ docker/app.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Создаю миграцию Alembic..."
+alembic revision --autogenerate -m "initial migration"
+echo "Миграция создана!"
+
 echo "Запуск миграций..."
 alembic upgrade head || { echo "Ошибка при выполнении миграций."; exit 1; }
+echo "Миграция прошла успешно!"
 
 # Запуск приложения
 gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
