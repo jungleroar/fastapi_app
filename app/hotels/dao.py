63d352b8de3b88f8a5cc412f6db804ff557bc703
onemanzerg <donnydeus@gmail.com>
2025-02-17 21:28:46 +0300

    code review

--- app/hotels/dao.py
+++ app/hotels/dao.py
@@ -1,73 +1,72 @@
-from app.dao.base import BaseDAO
-from app.hotels.models import Hotels
-from app.bookings.models import Bookings
-from app.hotels.rooms.models import Rooms
-from app.database import async_session_maker
-from sqlalchemy import select
 from datetime import date
 
+from sqlalchemy import and_, func, or_, select
 
-class HotelsDAO(BaseDAO):
+from app.bookings.models import Bookings
+from app.dao.base import BaseDAO
+from app.database import async_session_maker, engine
+from app.hotels.models import Hotels
+from app.hotels.rooms.models import Rooms
+
+
+class HotelDAO(BaseDAO):
     model = Hotels
 
     @classmethod
-    async def search_for_hotels(cls,
-                                location: str,
-                                date_from: date,
-                                date_to: date,
-                                ):
+    async def find_all(cls, location: str, date_from: date, date_to: date):
+
+        booked_rooms = (
+            select(Bookings.room_id, func.count(Bookings.room_id).label("rooms_booked"))
+            .select_from(Bookings)
+            .where(
+                or_(
+                    and_(
+                        Bookings.date_from >= date_from,
+                        Bookings.date_from <= date_to,
+                    ),
+                    and_(
+                        Bookings.date_from <= date_from,
+                        Bookings.date_to > date_from,
+                    ),
+                ),
+            )
+            .group_by(Bookings.room_id)
+            .cte("booked_rooms")
+        )
+
+        booked_hotels = (
+            select(Rooms.hotel_id, func.sum(
+                    Rooms.quantity - func.coalesce(booked_rooms.c.rooms_booked, 0)
+            ).label("rooms_left"))
+            .select_from(Rooms)
+            .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
+            .group_by(Rooms.hotel_id)
+            .cte("booked_hotels")
+        )
+
+        get_hotels_with_rooms = (
+            # Код ниже можно было бы расписать так:
+            # select(
+            #     Hotels
+            #     booked_hotels.c.rooms_left,
+            # )
+            # Но используется конструкция Hotels.__table__.columns. Почему? Таким образом алхимия отдает
+            # все столбцы по одному, как отдельный атрибут. Если передать всю модель Hotels и
+            # один дополнительный столбец rooms_left, то будет проблематично для Pydantic распарсить
+            # такую структуру данных. То есть проблема кроется именно в парсинге ответа алхимии 
+            # Пайдентиком.
+            select(
+                Hotels.__table__.columns,
+                booked_hotels.c.rooms_left,
+            )
+            .join(booked_hotels, booked_hotels.c.hotel_id == Hotels.id, isouter=True)
+            .where(
+                and_(
+                    booked_hotels.c.rooms_left > 0,
+                    Hotels.location.like(f"%{location}%"),
+                )
+            )
+        )
         async with async_session_maker() as session:
-            pass
-
-
-    # @classmethod
-    # async def find_hotels(cls,
-    #               hotel_id: int,
-    #               date_from: date,
-    #               date_to: date,
-    #               ):
-    #     async with async_session_maker() as session:
-
-    #         booked_rooms = select(Bookings).where(
-    #             and_(
-    #                 Bookings.hotel_id == hotel_id,
-    #                 or_(
-    #                     and_(
-    #                         Bookings.date_from >= date_from,
-    #                         Bookings.date_from <= date_to
-    #                     ),
-    #                     and_(
-    #                         Bookings.date_from <= date_from,
-    #                         Bookings.date_to > date_from
-    #                     ), 
-    #                 )
-    #             )
-    #         ).cte("booked_rooms")
-
-    #         get_rooms_left = select(Rooms.quantity - func.count(booked_rooms.c.room_id)
-    #                             ).select_from(Rooms).outerjoin(
-    #                                 booked_rooms, booked_rooms.c.room_id == Rooms.id,
-    #                             ).where(Rooms.id == room_id).group_by(
-    #                                 Rooms.quantity, booked_rooms.c.room_id
-    #                             )
-    #         rooms_left = await session.execute(get_rooms_left)
-    #         rooms_left: int = rooms_left.scalar()
-
-    #         if rooms_left > 0:
-    #             get_price = select(Rooms.price).filter_by(id=room_id)
-    #             price = await session.execute(get_price)
-    #             price: int = price.scalar()
-    #             add_booking = insert(Bookings).values(
-    #                 room_id=room_id,
-    #                 user_id=user_id,
-    #                 date_from=date_from,
-    #                 date_to=date_to,
-    #                 price=price,
-    #             ).returning(Bookings)
-
-    #             new_booking = await session.execute(add_booking)
-    #             await session.commit()
-    #             return new_booking.scalar()
-
-    #         else:
-    #             return None
+            hotels_with_rooms = await session.execute(get_hotels_with_rooms)
+            return hotels_with_rooms.mappings().all()
--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Query
-from app.hotels.dao import HotelsDAO
+from app.hotels.dao import HotelDAO
 from datetime import date, datetime
 from app.hotels.schemas import SHotel
 from fastapi_cache.decorator import cache
@@ -17,7 +17,7 @@ async def get_hotels_by_location_and_date(location: str,
                                           date_from: date = Query(..., description=f"Например, {datetime.now().date()}"),
                                           date_to: date = Query(..., description=f"Например, {datetime.now().date()}"),
                                         ):
-    hotels = await HotelsDAO.search_for_hotels(location, date_from, date_to)
+    hotels = await HotelDAO.search_for_hotels(location, date_from, date_to)
     # hotels_json = parse_obj_as(list[SHotel], hotels)
     await asyncio.sleep(3)
     return hotels
@@ -25,4 +25,17 @@ async def get_hotels_by_location_and_date(location: str,
 
 @router.get("/all")
 async def get_all_hotels():
-    return await HotelsDAO.find_all()
+    return await HotelDAO.find_all()
+
+
+@router.get("/id/{hotel_id}", include_in_schema=True)
+# Этот эндпоинт используется для фронтенда, когда мы хотим отобразить все
+# номера в отеле и информацию о самом отеле. Этот эндпоинт как раз отвечает за информацию
+# об отеле.
+# В нем нарушается правило именования эндпоинтов: конечно же, /id/ здесь избыточен.
+# Тем не менее, он используется, так как эндпоинтом ранее мы уже задали получение
+# отелей по их локации вместо id.
+async def get_hotel_by_id(
+    hotel_id: int,
+) -> Optional[SHotel]:
+    return await HotelDAO.find_one_or_none(id=hotel_id)
